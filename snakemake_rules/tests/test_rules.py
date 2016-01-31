9b385cadbc62d446d2ed15d510d741dba190d144
Per Unneberg <per.unneberg@scilifelab.se>
2016-01-31 21:52:56 +0100

    Add test data

---
+++ snakemake_rules/tests/test_rules.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2016 by Per Unneberg
+from os.path import abspath, dirname, join
+import logging
+import shutil
+import subprocess as sp
+import pytest
+
+logger = logging.getLogger(__name__)
+
+SNAKEFILE=join(abspath(dirname(__file__)), "Snakefile")
+
+
+def test_snakemake():
+    """Test snakemake command call"""
+    output = sp.check_output(['snakemake', '-s', SNAKEFILE, '-l'], stderr=sp.STDOUT)
+    assert "bwa_mem" in str(output)
+    
+
+def test_bwa_align():
+    """Test bwa alignment"""
+    output = sp.check_output(['snakemake', '-s', SNAKEFILE, '-F', 'data/test.bam'], stderr=sp.STDOUT)
+    print(output)
