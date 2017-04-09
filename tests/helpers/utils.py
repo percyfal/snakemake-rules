4e934ee6e41a306fddfa95adbe6e7d4737904338
Per Unneberg <per.unneberg@scilifelab.se>
2017-04-09 11:08:15 +0200

    Move parse rule to ruleinfo

--- tests/helpers/utils.py
+++ tests/helpers/utils.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2016 by Per Unneberg
-# Helper functions for parsning and for making output executable
+# Helper functions for parsing and for making output executable
 import os
 from os.path import abspath, dirname, join
 import re
@@ -8,72 +8,16 @@ import pytest
 import subprocess as sp
 import contextlib
 import yaml
+import logging
 from snakemake.parser import parse
+from snakemake.io import glob_wildcards, update_wildcard_constraints
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 TESTDIR = join(abspath(dirname(__file__)), os.pardir)
 with open(os.path.join(TESTDIR, "rules2target.yaml")) as fh:
     rules2targets = yaml.load(fh)
-
-# Map file extension to file extension fixture; keys are concatenated
-# with | and compiled to regular expression
-re_filetypes = {
-    "bam" : "bam",
-    "bai" : "bai",
-    "bed" : "bed",
-    "fa" : "fasta",
-    "fasta" : "fasta",
-    "fai" : "fastaindex",
-    "fofn" : "fofn",
-    "sam" : "sam",
-    "samtools_stats.txt" : "samtools_stats",
-    "vcf" : "vcf",
-    "vcf.gz" : "vcf",
-    "tbi" : "tabix",
-}
-regex = re.compile("\.({})\"".format("|".join("{}".format(x) for x in sorted(re_filetypes.keys()))))
-regex_input = re.compile("@workflow\.input\(\s+(?P<input>.*)")
-regex_output = re.compile("@workflow.output\(\s+(?P<output>.*)")
-
-def parse_rule(rule, prefix=None):
-    """Generate input/output information for rule.
-
-    Params:
-      rule (str): file containing a snakemake rule """
-    rn = os.path.basename(rule).replace(".rule", "")
-    app = os.path.basename(os.path.dirname(rule))
-    target = rules2targets.get(app, {}).get(rn, None)
-    if target:
-        return target
-    code, linemake, rulecount = parse(rule)
-    m = regex_input.search(code)
-    if m:
-        input = m.group("input")
-    m = regex_output.search(code)
-    if m is None:
-        # return input case
-        m = regex_input.search(code)
-        output = m.group("input")
-    else:
-        output = m.group("output")
-    m = regex.findall(input)
-    if m:
-        print(m)
-        #print(minput.groups())
-    else:
-        print("no match")
-    m = re.search("\"[ ]*(?P<prefix>\{[a-zA-Z_0-9]+\})+(?P<ext>[_\/\.a-zA-Z0-9 ]+)\"", output)
-    # Regular extension; use first one
-    if m:
-        return "{prefix}{ext}".format(prefix=prefix, ext=m.group("ext"))
-    # expand case; skip for now
-    m = re.search("expand", output)
-    if m:
-        return None
-    # Config case
-    m = re.search("[a-zA-Z =]*(?P<config>config[^\)]+)", output)
-    if m:
-        return "config"
-    return None
 
 
 def run(cmd, stdout=sp.PIPE, stderr=sp.STDOUT):
