1043a80cb9c9c34510cb934818edcf9b8d88d5fe
Per Unneberg <per.unneberg@scilifelab.se>
2017-04-15 15:49:57 +0200

    Improve parsing of sampleinfo file
    Add more information messages and add support for
    samples/ignore_samples.
    
    Fixes #30

--- snakemake_rules/core/utils.py
+++ snakemake_rules/core/utils.py
@@ -12,15 +12,30 @@ def get_samples(config, logger):
     try:
         ## Get the sample information
         _include = config.get('samples', [])
+        if _include is None:
+            _include = []
+        assert isinstance(_include, list), print("config['samples'] should be a list")
         _exclude = config.get('ignore_samples', [])
+        if _exclude is None:
+            _exclude = []
+        assert isinstance(_exclude, list), print("config['ignore_samples] should be a list")
         _ignored = []
         if len(_include) == 0:
             config['samples'] = []
         with open(config['settings']['sampleinfo']) as csvfile:
             dialect = csv.Sniffer().sniff(csvfile.read(10240), delimiters=[',',';'])
+            config['_sampleinfo_delim'] = dialect.delimiter # save for later use by QC rules
             csvfile.seek(0)
             reader = csv.DictReader(csvfile, dialect=dialect)
             rows = [row for row in reader]
+            # check if sample in config but not in sampleinfo:
+            if len(_include)>0 and len([s for s in _include if s not in [row['SM'] for row in rows]])>0:
+                logger.info("[snakemake_rules.settings]: warning: sample listed in config['samples'] not in '{}'".format(config['settings']['sampleinfo']))
+            if len(_exclude)>0 and len([s for s in _exclude if s not in [row['SM'] for row in rows]])>0:
+                logger.info("[snakemake_rules.settings]: warning: sample listed in config['ignore_samples'] not in '{}'".format(config['settings']['sampleinfo']))
+            # check overlap include exclude:
+            if len(_include)>0 and len(_exclude)>0 and not set(_include).isdisjoint(_exclude):
+                logger.info("[snakemake_rules.settings]: warning: same sample(s) listed in config['samples'] and config['ignore_samples']")
             config['_sampleinfo'] = []
             for row in rows:
                 # Only excluded samples listed
@@ -36,10 +51,12 @@ def get_samples(config, logger):
                     logger.warning("[snakemake_rules.settings]: {} neither in include or exclude section of sampleinfo file".format(row['SM']))
             for s in _ignored:
                 logger.info("[snakemake_rules.settings]: ignoring sample {} in current run".format(s))
+        logger.info("[snakemake_rules.settings]: successfully loaded {} sample(s) from '{}'".format(len(config['_sampleinfo']), config['settings']['sampleinfo']))
         if len(_include) == 0:
             config['samples'] = sorted(list(set(config['samples'])))
             logger.info("[snakemake_rules.settings]: no samples listed in config; added {} samples to config['samples']".format(len(config['samples'])))
-        logger.info("[snakemake_rules.settings]: successfully loaded {} entries from '{}'".format(len(config['_sampleinfo']), config['settings']['sampleinfo']))
+        if len(_ignored) > 0:
+            logger.info("[snakemake_rules.settings]: ignored {} sample(s) listed in '{}'".format(len(_ignored), config['settings']['sampleinfo']))        
     except Exception as e:
         logger.info("[snakemake_rules.settings]: parsing sampleinfo failed; not setting config['_sampleinfo']: {}".format(e))
 
